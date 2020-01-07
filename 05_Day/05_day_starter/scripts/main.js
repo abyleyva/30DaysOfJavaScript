87e7ab1496c78520dc6c8ffbba21f71bce2ee82b
Aby Leyva <abyleyva75@gmail.com>
2020-01-07 12:03:08 -0600

    changes for me

--- 05_Day/05_day_starter/scripts/main.js
+++ 05_Day/05_day_starter/scripts/main.js
@@ -1,4 +1,4 @@
 console.log(countries)
-alert('Open the browser console whenever you work on JavaScript')
+alert('Open the browser console whenever you work on JavaScript, add new text')
 alert('Open the console and check if the countries has been loaded')
-console.log('chage make for me')
+console.log('chage make for me. One more time')
